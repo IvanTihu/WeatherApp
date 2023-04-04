5a82571eb6be06ddae5c6c8e4c0f3d92fe9f837f
Ivan <ivantihu@gmail.com>
2023-04-04 18:13:28 +0300

    tweak Adapter

--- app/src/main/java/com/example/weatherapp/adapters/WeatherAdapter.kt
+++ app/src/main/java/com/example/weatherapp/adapters/WeatherAdapter.kt
@@ -1,11 +1,15 @@
 package com.example.weatherapp.adapters
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.weatherapp.R
 import com.example.weatherapp.databinding.ListItemBinding
 
-class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder>() {
+class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder>(Comparator()) {
 
     class Holder(view: View) : RecyclerView.ViewHolder(view) {
         val binding = ListItemBinding.bind(view)
@@ -14,5 +18,28 @@ class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder>() {
             tvCondition.text = item.condition
             tvTemp.text = item.currentTemp
         }
+    }
+
+    class Comparator: DiffUtil.ItemCallback<WeatherModel>() {
+
+        override fun areItemsTheSame(oldItem: WeatherModel, newItem: WeatherModel): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: WeatherModel, newItem: WeatherModel): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        return Holder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.list_item, parent,false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(getItem(position))
     }
 }
