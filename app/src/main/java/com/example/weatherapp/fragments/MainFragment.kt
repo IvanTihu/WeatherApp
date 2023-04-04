7adc8a2fa543f2fbdf266bc3ca885426226dfa94
Ivan <ivantihu@gmail.com>
2023-04-04 19:36:17 +0300

    make test request

--- app/src/main/java/com/example/weatherapp/fragments/MainFragment.kt
+++ app/src/main/java/com/example/weatherapp/fragments/MainFragment.kt
@@ -2,6 +2,7 @@ package com.example.weatherapp.fragments
 
 import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,12 +12,16 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.FragmentActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.weatherapp.R
 import com.example.weatherapp.adapters.VpAdapter
 import com.example.weatherapp.databinding.FragmentMainBinding
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
+const val API_KEY = "c254aa7655f74892b46132829232803"
 
 class MainFragment : Fragment() {
 
@@ -25,7 +30,7 @@ class MainFragment : Fragment() {
     private val fList = listOf(
         HoursFragment.newInstance(), DaysFragment.newInstance()
     )
-    private val tList = listOf("ore", "Zile")
+    private val tList = listOf("Hours", "Days")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -40,6 +45,7 @@ class MainFragment : Fragment() {
 
         checkPermission()
         init()
+        requestWeatherData("London")
     }
 
     private fun init() = with(binding) {
@@ -62,6 +68,25 @@ class MainFragment : Fragment() {
             permissionListener()
             pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
+    }
+
+    private fun requestWeatherData(cityName: String) {
+        val url = "https://api.weatherapi.com/v1/forecast.json?key=" +
+                API_KEY +
+                "&q=$cityName&days=3&aqi=no&alerts=no"
+
+        val queue = Volley.newRequestQueue(context)
+        val request = StringRequest(
+            Request.Method.GET,
+            url,
+            { result ->
+                Log.d("MyLog", "Result: $result")
+            },
+            { error ->
+                Log.d("MyLog", "Error: $error")
+            }
+        )
+        queue.add(request)
     }
 
     companion object {
